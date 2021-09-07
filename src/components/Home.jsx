f7815861f31752cea4bd6a55668ac8c47288900c
Francois Janson <jansonfrancois@gmail.com>
2021-09-07 16:58:40 +0200

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 class Home extends React.Component {
   render() {
     return (
-      <ul>
+      <>
         {this.props.movies.map((elem) => (
-          <li key={elem.id.toString()}>
-            <Link to={`movie/${elem.id}`}>{elem.title}</Link>
-          </li>
+          <Link to={`movie/${elem.id}`}>
+            <img src={elem.image} alt="" />
+          </Link>
         ))}
-      </ul>
+      </>
     );
   }
 }
